57fff38593d1715efa88d32636918c2244849ffd
bastih <sebastian.hillig@gmail.com>
2013-07-16 14:11:17 +0000

    Remove Strategy from TableFactory

--- src/bin/units_storage/table_factory_tests.cpp
+++ src/bin/units_storage/table_factory_tests.cpp
@@ -8,7 +8,7 @@
 class TableFactoryTests : public ::hyrise::Test {};
 
 TEST_F(TableFactoryTests, number_of_column) {
-  TableFactory<MemalignStrategy<64> > t;
+  TableFactory t;
   std::vector<const ColumnMetadata *> meta;
   meta.push_back(new ColumnMetadata("basti", IntegerType));
   hyrise::storage::atable_ptr_t  table = t.generate(&meta);
